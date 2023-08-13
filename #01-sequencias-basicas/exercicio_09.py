3f8c78a28ff4273386c48c7034f0efd668e096dc
Marcos Pontes Junior <127991089+marcospontesjunior@users.noreply.github.com>
2023-08-13 22:54:08 +0100

    Delete #01-sequencias-basicas directory

--- #01-sequencias-basicas/exercicio_09.py
+++
@@ -1,8 +0,0 @@
-"""Faça um algoritmo que leia quanto dinheiro uma pessoa tem na carteira (em R$) 
-e mostre quantos dólares ela pode comprar. Considere US$1,00 = R$3,45."""
-
-valor = float(input("Digite o valor para ser convertido: "))
-
-converter = valor * 3.45
-
-print(f"O valor R${valor} convertido para Dólar é de US${converter:.2f}")
--- #01-sequencias-basicas/exercicio_13.py
+++
@@ -1,9 +0,0 @@
-"""Faça um algoritmo que leia o salário de um funcionário, calcule e mostre o 
-seu novo salário, com 15% de aumento."""
-
-entrada_salario = float(input("Digite o atual salário do funcionário: "))
-
-calco_aumento = entrada_salario * 0.15
-salario_final = entrada_salario + calco_aumento
-
-print(f"O valor final do salário do funcionário com o aumento de 15% é de R${salario_final:.2f}")
