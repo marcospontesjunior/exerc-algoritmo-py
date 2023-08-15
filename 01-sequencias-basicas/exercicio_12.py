01d354ea650a8f7ca43f86f592ef127f1936ab6a
Marcos Pontes Junior <127991089+marcospontesjunior@users.noreply.github.com>
2023-08-15 19:22:49 +0100

    Delete 01-sequencias-basicas directory

--- 01-sequencias-basicas/exercicio_12.py
+++
@@ -1,9 +0,0 @@
-"""Crie um programa que leia o preço de um produto, calcule e mostre o seu 
-PREÇO PROMOCIONAL, com 5% de desconto."""
-
-preco_produto = float(input("Digite o preço do produto: "))
-
-desconto = preco_produto * 0.05
-valor_final = preco_produto - desconto
-
-print(f"O valor do produto é R${preco_produto:.2f}. Com 5% de desconto o valor final do produto é de: R${valor_final:.2f}")
